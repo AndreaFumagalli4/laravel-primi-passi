170a29bb680d92925964d52f6886dd10a1975458
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-07 15:13:18 +0100

    add products.blade.php view

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,16 +4,13 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Laravel primi passi</title>
+        <title>Products</title>
     </head>
     <body>
-        <header>
-            <h1>
-                Laravel primi passi
-            </h1>
+        <main>
             <h2>
-                Homepage
+                Products
             </h2>
-        </header>
+        </main>
     </body>
 </html>
